d0c5fb2f8b14bed9360055f687a04d570a5d11fd
David Dias <daviddias.p@gmail.com>
2015-11-23 12:20:08 +0100

    so fast

--- src/ibs.js
+++ src/ibs.js
@@ -1,19 +1,6 @@
 module.exports = function (baseDir) {
-  var mfs = require('ipfs-blob-store')().mfs
-  console.log('BASEDIR->', baseDir)
+  var mfs = require('ipfs-blob-store')()
+  // console.log('BASEDIR->', baseDir)
   mfs.baseDir = baseDir
   return mfs
-
-  /*
-  if (baseDir) {
-    console.log('BASEDIR->', baseDir)
-    mfs.baseDir = baseDir
-    mfs.node.files.mkdir(mfs.baseDir, { p: true }, function (err) {
-      if (err) {
-        return console.error(err)
-      }
-    })
-  }
-  return mfs
-  */
 }

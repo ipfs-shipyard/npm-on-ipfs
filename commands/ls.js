1ecf84b19b34f4ff9539e1a1388e478f79a99cb4
David Dias <daviddias.p@gmail.com>
2015-11-30 15:40:23 +0000

    add ls

---
+++ commands/ls.js
@@ -0,0 +1,37 @@
+var Command = require('ronin').Command
+var fetchIPNS = require('../src/fetch-ipns.js')
+var ipfsAPI = require('ipfs-api')
+
+module.exports = Command.extend({
+  desc: 'Check modules available in the mirror',
+
+  options: {
+    update: {
+      type: 'boolean',
+      alias: 'u',
+      default: false
+    }
+  },
+
+  run: function (update, name) {
+    if (update) {
+      fetchIPNS({ blobStore: true }, function () {
+        ls()
+      })
+    } else {
+      ls()
+    }
+
+    function ls () {
+      var apiCtl = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
+      apiCtl.files.ls('/npm-registry', function (err, res) {
+        if (err) {
+          return console.log(err)
+        }
+        res.Entries.forEach(function (module) {
+          console.log(module.Name, '\t', module.Hash)
+        })
+      })
+    }
+  }
+})

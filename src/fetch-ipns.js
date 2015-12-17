916849ca67ff1854a52fa9051eb497990ce73de4
David Dias <daviddias.p@gmail.com>
2015-12-17 16:53:05 +0000

    move from castor to biham

--- src/fetch-ipns.js
+++ src/fetch-ipns.js
@@ -8,6 +8,10 @@ var updaterNodes = {
   castor: {
     addr: '/ip4/37.59.33.238/tcp/4001/ipfs/QmdNc4B89DxVeiuDKRN5bWdKsAPCmekgmJMkRSdUNa7x9z',
     ipns: '/ipns/QmdNc4B89DxVeiuDKRN5bWdKsAPCmekgmJMkRSdUNa7x9z'
+  },
+  biham: {
+    addr: '/ip4/188.40.114.11/tcp/4001/ipfs/QmZY7MtK8ZbG1suwrxc7xEYZ2hQLf1dAWPRHhjxC8rjq8E',
+    ipns: '/ipns/QmZY7MtK8ZbG1suwrxc7xEYZ2hQLf1dAWPRHhjxC8rjq8E'
   }
 }
 
@@ -23,11 +27,11 @@ function fetchIPNS (config, callback) {
   if (!config.clone && config.blobStore) {
     var apiCtl = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
 
-    apiCtl.swarm.connect(updaterNodes.castor.addr, function (err) {
+    apiCtl.swarm.connect(updaterNodes.biham.addr, function (err) {
       if (err) {
         return callback(err)
       }
-      copyNpmRegistry(apiCtl, updaterNodes.castor.ipns, callback)
+      copyNpmRegistry(apiCtl, updaterNodes.biham.ipns, callback)
     })
   }
 }

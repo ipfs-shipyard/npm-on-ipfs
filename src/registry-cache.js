e600c99d31aafc1f3fe14ae78f0aaeb2040b4951
David Dias <daviddias.p@gmail.com>
2016-02-02 23:04:05 +0000

    pass the callback func directly to the flush function of waterfal

--- src/registry-cache.js
+++ src/registry-cache.js
@@ -3,57 +3,56 @@ const log = debug('registry-mirror')
 const async = require('async')
 log.error = debug('registry-mirror:error')
 const config = require('./config')
+const api = config.apiCtl
 
 exports = module.exports
 
 exports.connect = (callback) => {
   // TODO support connection to more than one node
   // TODO check which nodes are missing from the list first
-  config.apiCtl.swarm.connect(config.nodes.biham, callback)
+  api.swarm.connect(config.nodes.biham, callback)
 }
 
 exports.cacheRegistry = (callback) => {
   async.waterfall([
     (cb) => {
-      config.apiCtl.name.resolve(config.registryHash, (err, res) => {
+      api.name.resolve(config.registryHash, (err, res) => {
         cb(err, res.Path)
       })
     },
-    config.apiCtl.cat,
+    api.cat,
     (stream, cb) => {
-      config.apiCtl.block.put(stream, (err, res) => {
+      api.block.put(stream, (err, res) => {
         cb(err, '/ipfs/' + res.Key)
       })
     },
     (ipfsHash, cb) => {
-      config.apiCtl.files.mv(['/npm-registry', '/npm-registry.bak-' + Date.now().toString()], (err) => {
+      api.files.mv(['/npm-registry', '/npm-registry.bak-' + Date.now().toString()], (err) => {
         cb(err, ipfsHash)
       })
     },
     (ipfsHash, cb) => {
-      config.apiCtl.files.cp([ipfsHash, '/npm-registry'], (err) => {
+      api.files.cp([ipfsHash, '/npm-registry'], (err) => {
         cb(err, ipfsHash)
       })
     }
-  ], (err, ipfsHash) => {
-    callback(err, ipfsHash)
-  })
+  ], callback)
 }
 
 exports.publish = () => {
-  config.apiCtl.files.stat('/npm-registry', function (err, res) {
+  api.files.stat('/npm-registry', function (err, res) {
     if (err) {
       return log.err('stat', err)
     }
-    config.apiCtl.block.get(res.Hash, function (err, stream) {
+    api.block.get(res.Hash, function (err, stream) {
       if (err) {
         return log.err('block get', err)
       }
-      config.apiCtl.add(stream, function (err, res) {
+      api.add(stream, function (err, res) {
         if (err) {
           return log.err('add', err)
         }
-        config.apiCtl.name.publish('/ipfs/' + res[0].Hash, function (err, res) {
+        api.name.publish('/ipfs/' + res[0].Hash, function (err, res) {
           if (err) {
             return log.err('name publish', err)
           }

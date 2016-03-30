517ffe8fccac3e39117187d4188e5377e5046ded
David Dias <daviddias.p@gmail.com>
2016-03-30 10:05:22 -0400

    update ipns link

--- src/config.js
+++ src/config.js
@@ -11,9 +11,9 @@ module.exports = {
   log: log,
   apiCtl: ipfsAPI('/ip4/127.0.0.1/tcp/5001'),
   nodes: {
-    biham: '/ip4/188.40.114.11/tcp/4001/ipfs/QmZY7MtK8ZbG1suwrxc7xEYZ2hQLf1dAWPRHhjxC8rjq8E'
+    biham: '/ip4/188.40.114.11/tcp/8801/ipfs/QmToeN85brexqyXUnWnKfHFqhvucJPViw9AxQQkjLoULy4'
   },
-  registryRecord: '/ipns/QmZY7MtK8ZbG1suwrxc7xEYZ2hQLf1dAWPRHhjxC8rjq8E',
+  registryRecord: '/ipns/QmToeN85brexqyXUnWnKfHFqhvucJPViw9AxQQkjLoULy4',
   mirror: {
     port: '9876',
     host: '127.0.0.1'

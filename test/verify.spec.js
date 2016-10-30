30967f63ff9dd0282bda50beb16433c0d1e9eec6
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-10-30 13:55:25 +0100

    more tests for verify

--- test/verify.spec.js
+++ test/verify.spec.js
@@ -1,3 +1,4 @@
+/* eslint max-nested-callbacks: ["error", 8] */
 /* eslint-env mocha */
 'use strict'
 
@@ -6,9 +7,10 @@ chai.use(require('sinon-chai'))
 const sinon = require('sinon')
 
 const expect = chai.expect
-const mockery = require('mockery')
 const crypto = require('crypto')
 const fs = require('fs')
+const path = require('path')
+const Writable = require('stream').Writable
 
 function clearData (done) {
   memblob.data = {'existing.tgz': 'asdf'}
@@ -17,22 +19,12 @@ function clearData (done) {
 
 const memblob = require('abstract-blob-store')()
 
-describe('verify', () => {
+describe.only('Verifier', () => {
   let verifier
 
   before(() => {
-    mockery.enable({
-      useCleanCache: true,
-      warnOnReplace: false,
-      warnOnUnregistered: false
-    })
     const Verifier = require('../src/ipfs-npm/registry/clone/verifier')
     verifier = new Verifier(memblob)
-  })
-
-  after(() => {
-    mockery.deregisterAll()
-    mockery.disable()
   })
 
   describe('.verify', () => {
@@ -50,7 +42,7 @@ describe('verify', () => {
 
     beforeEach(clearData)
 
-    it("checks hash and does't call update", (done) => {
+    it("checks hash and doesn't call update", (done) => {
       const info = {
         path: '/the/path/1',
         tarball: 'existing.tgz',
@@ -74,6 +66,102 @@ describe('verify', () => {
       verifier.verify(info, (err, d) => {
         expect(err).to.not.exist
         expect(verifier.update).to.have.been.calledOnce
+        done()
+      })
+    })
+  })
+
+  describe('.update', () => {
+    it('errors on missing tarball', (done) => {
+      verifier.update({path: 'mypath', shasum: 'checksum'}, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).be.eql('insufficient data')
+        done()
+      })
+    })
+
+    it('errors on missing path', (done) => {
+      verifier.update({tarball: 'mypath.tgz', shasum: 'checksum'}, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).be.eql('insufficient data')
+        done()
+      })
+    })
+
+    it('errors on missing shasum', (done) => {
+      verifier.update({tarball: 'mypath.tgz', path: 'mypath'}, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).be.eql('insufficient data')
+        done()
+      })
+    })
+
+    it('errors on invalid sha checksum', (done) => {
+      const info = {
+        path: '/module-best-practices/-/module-best-practices-1.1.23.tgz',
+        tarball: 'module-best-practices-1.1.23.tgz',
+        shasum: '_e72aadf604d52983adaa087f7bf3371ef6bd6ac1'
+      }
+
+      verifier.update(info, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).to.match(/failed to verify shasum/)
+        done()
+      })
+    })
+
+    it('errors on non existent path', (done) => {
+      const info = {
+        path: '/module-best-practices/-/module-best-practices-0.0.0.tgz',
+        tarball: 'module-best-practices-0.0.0.tgz',
+        shasum: 'e72aadf604d52983adaa087f7bf3371ef6bd6ac1'
+      }
+
+      verifier.update(info, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).to.match(/failed to download/)
+        done()
+      })
+    })
+
+    it('errors on failed write to blob store', (done) => {
+      const info = {
+        path: '/module-best-practices/-/module-best-practices-1.1.23.tgz',
+        tarball: 'module-best-practices-1.1.23.tgz',
+        shasum: 'e72aadf604d52983adaa087f7bf3371ef6bd6ac1'
+      }
+
+      sinon
+        .stub(memblob, 'createWriteStream', (file, cb) => {
+          process.nextTick(() => cb(new Error('fail')))
+          return Writable()
+        })
+
+      verifier.update(info, (err) => {
+        expect(err).to.be.an('error')
+        expect(err.message).to.match(/failed to save/)
+        memblob.createWriteStream.restore()
+        done()
+      })
+    })
+
+    it('stores the fetched tarball and data', (done) => {
+      memblob.data = {}
+      const info = {
+        path: '/module-best-practices/-/module-best-practices-1.1.23.tgz',
+        tarball: 'module-best-practices-1.1.23.tgz',
+        shasum: 'e72aadf604d52983adaa087f7bf3371ef6bd6ac1'
+      }
+
+      verifier.update(info, (err) => {
+        expect(err).to.not.exist
+        expect(memblob.data).to.have.key('module-best-practices-1.1.23.tgz')
+
+        expect(
+          memblob.data['module-best-practices-1.1.23.tgz']
+        ).to.be.eql(
+          fs.readFileSync(path.join(__dirname, 'fixtures/module-best-practices-1.1.23.tgz'))
+        )
         done()
       })
     })

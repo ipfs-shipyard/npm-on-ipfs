81b386ba4a28a8a0d8d5ae8eee934f96f36b9f44
achingbrain <alex@achingbrain.net>
2018-10-26 17:43:34 +0100

    test: add test for yarn

--- test/install.spec.js
+++ test/install.spec.js
@@ -26,11 +26,46 @@ describe('install', function () {
     await fs.copyFile(path.resolve(__dirname, './fixtures/package.json'), path.join(projectDirectory, 'package.json'))
   })
 
-  it('should install a project', (done) => {
+  it('should install a project with npm', (done) => {
     const installer = spawn(
       process.argv[0], [
         path.resolve(__dirname, '../src/cli/bin.js'),
-        `--ipfs-node=disposable`,
+        '--ipfs-node=disposable',
+        '--package-manager=npm',
+        'install'
+      ], {
+        cwd: projectDirectory
+      })
+
+    const buffer = new OutputBuffer((line) => {
+      console.info(line) // eslint-disable-line no-console
+    })
+
+    installer.stdout.on('data', (data) => {
+      buffer.append(data.toString())
+    })
+
+    installer.stderr.on('data', (data) => {
+      buffer.append(data.toString())
+    })
+
+    installer.on('close', async (code) => {
+      buffer.flush()
+
+      if (code === 0) {
+        return done()
+      }
+
+      done(new Error(`Unexpected exit code ${code}`))
+    })
+  })
+
+  it('should install a project with yarn', (done) => {
+    const installer = spawn(
+      process.argv[0], [
+        path.resolve(__dirname, '../src/cli/bin.js'),
+        '--ipfs-node=disposable',
+        '--package-manager=yarn',
         'install'
       ], {
         cwd: projectDirectory
